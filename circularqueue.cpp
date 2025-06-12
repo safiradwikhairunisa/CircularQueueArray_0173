ae2469a0a4414178138aef266cdd5edf3c416e78
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-06-12 10:46:58 +0700

    membuat if pada iterasi dari FRONT hingga REAR

--- circularqueue.cpp
+++ circularqueue.cpp
@@ -108,6 +108,16 @@ class queues
 
             //iterasi dari awal array hingga REAR
             while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+            cout << endl;
         }
     }
+};
+
+int main()
+{
+    
 }
