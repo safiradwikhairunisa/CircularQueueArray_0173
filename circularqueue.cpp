99a1c5dcbe5b876ba24c963ffe5754f22678e9fa
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-06-12 10:39:52 +0700

    Set FRONT_position and REAR_position from FRONT and REAR

--- circularqueue.cpp
+++ circularqueue.cpp
@@ -71,4 +71,34 @@ class queues
             FRONT = FRONT + 1;
         }
     }
+
+    void display()
+    {
+        int FRONT_position = FRONT;
+        int REAR_position = REAR;
+
+        //cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue is empty\n";
+            return;
+        }
+
+        cout << "\nElement in the queue are...\n";
+
+        //jika FRONT_position == are_position, iterasi dari FRONT hingga REAR
+        if (FRONT_position == REAR_position)
+        {
+            while (FRONT_position == REAR_position)
+            {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+        else
+        {
+            
+        }
+    }
 }
