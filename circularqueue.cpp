ff1e96c7ccbd76969359dda78021a09a2812e76b
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-06-12 10:23:09 +0700

    cek antrian pada void remove

--- circularqueue.cpp
+++ circularqueue.cpp
@@ -45,4 +45,14 @@ class queues
         }
         queue_array[REAR] = num;
     }
+
+    void remove()
+    {
+        //cek apakah antrian kosong
+        if (FRONT == - 1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+    }
 }
