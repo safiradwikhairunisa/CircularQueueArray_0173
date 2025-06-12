30e2bbe858a432b5bd16032d7769b7bec0f54b89
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-06-12 11:02:58 +0700

    add case 1-4

--- circularqueue.cpp
+++ circularqueue.cpp
@@ -134,6 +134,38 @@ int main()
             cout << "Enter your choice (1-4): ";
             cin >> ch;
             cout << endl;
+
+            switch (ch)
+            {
+                case '1':
+                {
+                    q.insert();
+                    break;
+                }
+                case '2':
+                {
+                    q.remove();
+                    break;
+                }
+                case '3':
+                {
+                    q.display();
+                    break;
+                }
+                case '4':
+                {
+                    return 0;
+                }
+                default:
+                {
+                    cout << "Invalid option!!" << endl;
+                    break;
+                }
+            }
+        }
+        catch (exception &e)
+        {
+            cout << "Check for the values entered" << endl;
         }
     }
 }
