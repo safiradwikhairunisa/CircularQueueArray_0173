564bb0e0f97568b7032b02da09f2a59a51a30a1c
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-06-12 10:19:20 +0700

    lihat posisi rear

--- circularqueue.cpp
+++ circularqueue.cpp
@@ -35,5 +35,14 @@ class queues
             FRONT = 0; //2.a
             REAR = 0; //2.b
         }
+        else
+        {
+            //jika REAR berada di posisi terakhir array, kembali keawal
+            if (REAR == max - 1)
+                REAR = 0;
+            else 
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
     }
 }
