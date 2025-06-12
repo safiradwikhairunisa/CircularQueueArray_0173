be2e6517bb58da2a981e3d761d79057c58ffe5ce
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-06-12 10:43:25 +0700

    cek apakah antrian kosong

--- circularqueue.cpp
+++ circularqueue.cpp
@@ -98,7 +98,16 @@ class queues
         }
         else
         {
-            
+            //jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+            while (FRONT_position == max - 1)
+            {
+                cout << queue_array[FRONT_position] << "   ";
+                FRONT_position++;
+            }
+            FRONT_position= 0;
+
+            //iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position)
         }
     }
 }
